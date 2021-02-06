b6eeb0ba07ad54c2e465d74dc3663b581ebd223e
CHANDRAKANT Kumar <kumar.chandrakant@soprabanking.com>
2021-02-07 02:18:38 +0530

    Adding daily batch of work.

---
+++ Framework Projects/java-seedstack/src/main/java/com/sapient/learning/interfaces/rest/HelloResource.java
@@ -0,0 +1,12 @@
+package com.sapient.learning.interfaces.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("hello")
+public class HelloResource {
+    @GET
+    public String hello() {
+        return "Hello World!";
+    }
+}
